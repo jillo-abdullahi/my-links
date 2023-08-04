93f5d6f009d376beff652e65abb81a62bab404fc
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-08-04 15:50:51 +0300

    feat - set up Swagger documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger docs
+  const config = new DocumentBuilder()
+    .setTitle('My Links API')
+    .setDescription(
+      'An API for developers to consolidate and share their links.',
+    )
+    .setVersion('1.0')
+    .addTag('nestjs, api, links, developers, postgresql, prisma')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   // use pipe validation globally
   // use whitelist to remove unwanted properties
   app.useGlobalPipes(
