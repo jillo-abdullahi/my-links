5a1d8fbb6fcaeeec77a0c0ce8ecbc92cc8448b31
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-08-09 18:37:41 +0300

    feat - add profile navbar and make it responsive

--- client/src/components/LogoImage.vue
+++ client/src/components/LogoImage.vue
@@ -1,13 +1,24 @@
 <template>
-    <div class="flex items-center justify-center">
-        <img :src="logoImagePath" alt="logo image" />
-        <img :src="logoTextPath" alt="logo text" />
+    <div class="flex items-center justify-center space-x-1">
+        <div class="shrink-0 min-w-8">
+            <img :src="logoImagePath" alt="logo image" :class="{ 'w-8': isInsideNav, }" />
+        </div>
+        <div class="shrink-0" :class="{ 'md:block hidden': isInsideNav }">
+            <img :src="logoTextPath" alt="logo text" :class="{ 'h-5': isInsideNav }" />
+        </div>
+
     </div>
 </template>
 
 <script lang="ts">
 export default {
     name: "LogoImage",
+    props: {
+        isInsideNav: {
+            type: Boolean,
+            default: false,
+        }
+    },
     data() {
         return {
             logoImagePath: require("@/assets/logo.svg"),
