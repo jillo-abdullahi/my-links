49903153853be3ac30e0d7d35c588f76751fb91d
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-08-09 19:50:39 +0300

    feat - add customize links section

---
+++ client/src/views/profile/CustomizeLinks.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-white rounded-lg p-4">This where you CustomizeLinks</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "CustomizeLinks",
+});
+</script>
--- client/src/views/profile/ProfileView.vue
+++ client/src/views/profile/ProfileView.vue
@@ -2,8 +2,13 @@
     <div class="-mt-12">
         <div class="p-6">
             <NavBar />
-            <div class="pt-6">
-                <MobilePreview />
+            <div class="pt-6 grid grid-cols-12 gap-x-6">
+                <div class="col-span-5">
+                    <MobilePreview />
+                </div>
+                <div class="col-span-7">
+                    <CustomizeLinks />
+                </div>
             </div>
         </div>
 
@@ -14,12 +19,14 @@
 import { defineComponent } from "vue";
 import NavBar from "./NavBar.vue";
 import MobilePreview from "./MobilePreview.vue";
+import CustomizeLinks from "./CustomizeLinks.vue";
 
 export default defineComponent({
     name: "ProfileView",
     components: {
         NavBar,
         MobilePreview,
+        CustomizeLinks,
     },
 });
 </script>
