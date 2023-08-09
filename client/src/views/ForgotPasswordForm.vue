5b2c72ba7fa847a6251e52bfa24ed34045b23eca
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-08-09 15:48:40 +0300

    feat - add validation and styles to forgot password form

--- client/src/views/ForgotPasswordForm.vue
+++ client/src/views/ForgotPasswordForm.vue
@@ -10,11 +10,11 @@
                 <p class="text-gray-600 mt-2">We'll send a reset link to your email address.</p>
             </div>
             <div class="space-y-6 mb-2">
-                <form class="space-y-6">
-                    <InputField type="email" name="email" id="email" placeholder="Enter your email" :icon="EmailIcon"
-                        label="Email address" />
+                <form class="space-y-6" @submit.prevent="submitForm">
+                    <InputField type="email" name="email" id="email" placeholder="Enter your email" label="Email address"
+                        @input="setValue" :error="error.email" />
 
-                    <ButtonPrimary text="Send Link" />
+                    <ButtonPrimary text="Send Link" :disabled="disableSubmitButton" />
                 </form>
             </div>
 
@@ -24,12 +24,12 @@
 </template>
   
 <script lang="ts">
+import { defineComponent } from "vue";
 import InputField from "../components/InputField.vue";
-import EmailIcon from "../assets/icons/EmailIcon.vue";
 import ButtonPrimary from "../components/ButtonPrimary.vue";
 import LogoImage from "../components/LogoImage.vue";
 
-export default {
+export default defineComponent({
     name: "ForgotPasswordForm",
     components: {
         InputField,
@@ -38,10 +38,37 @@ export default {
     },
     data() {
         return {
-            EmailIcon,
+            email: '',
+            error: {
+                email: '',
+            }
         }
-    }
-};
+    },
+    watch: {
+        email() {
+            this.error.email = ''; // reset error message
+        }
+    },
+    computed: {
+        disableSubmitButton(): boolean {
+            return Boolean(this.error.email);
+        }
+    },
+    methods: {
+        submitForm() {
+            if (!this.email) {
+                this.error.email = 'Email cannot be empty';
+            } else {
+                this.error.email = '';
+            }
+            console.log('submit form', this.email);
+        },
+        setValue(event: InputEvent) {
+            this.email = (event.target as HTMLInputElement).value;
+        },
+    },
+
+});
 </script>
   
 
