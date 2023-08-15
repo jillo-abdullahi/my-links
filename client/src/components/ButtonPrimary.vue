e9f37f43ab5219d46146e3af1abda58bad5088db
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-08-15 13:14:02 +0300

    feat - add save links button

--- client/src/components/ButtonPrimary.vue
+++ client/src/components/ButtonPrimary.vue
@@ -2,7 +2,7 @@
     <button class="text-white font-bold py-2 px-4 rounded-lg w-full" :disabled="disabled" :class="{
         'cursor-not-allowed hover:bg-purple-100 bg-purple-100': disabled,
         'cursor-pointer bg-purple-700 hover:bg-purple-300': !disabled
-    }">
+    }" @click="$emit('buttonClicked')">
         {{ text }}
     </button>
 </template>
--- client/src/views/profile/CustomizeLinks.vue
+++ client/src/views/profile/CustomizeLinks.vue
@@ -1,44 +1,51 @@
 <template>
-    <div class="bg-white rounded-lg p-10 w-full min-h-[720px]">
-        <div class="text-left">
-            <h1 class="text-4xl font-bold text-gray-700 pb-2">Customize your links</h1>
-            <h2 class="text-gray-400 text-base">Add/edit/remove links below and then share all your profiles with the world!
-            </h2>
-        </div>
-        <div class="w-full mt-10">
-            <ButtonSecondary text="+ Add new link" :is-inside-nav="false" @button-clicked="addNewLink" />
-        </div>
-        <div v-if="links.length < 1" class="mt-10 rounded-lg bg-gray-100 flex flex-col items-center justify-center py-10">
-            <LinksEmptyStateIcon />
-
-            <div class="mt-10 text-3xl font-bold text-gray-700">
-                Let’s get you started
+    <div class="bg-white rounded-lg min-h-[720px] flex flex-col justify-between items-center">
+        <div class="p-10 w-full ">
+            <div class="text-left">
+                <h1 class="text-4xl font-bold text-gray-700 pb-2">Customize your links</h1>
+                <h2 class="text-gray-400 text-base">Add/edit/remove links below and then share all your profiles with the
+                    world!
+                </h2>
             </div>
-            <div class="text-base text-gray-400 pt-6 w-full max-w-lg">
-                Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit
-                them. We’re here to help you share your profiles with everyone!
+            <div class="w-full mt-10">
+                <ButtonSecondary text="+ Add new link" :is-inside-nav="false" @button-clicked="addNewLink" />
+            </div>
+            <div v-if="links.length < 1"
+                class="mt-10 rounded-lg bg-gray-100 flex flex-col items-center justify-center py-10">
+                <LinksEmptyStateIcon />
+
+                <div class="mt-10 text-3xl font-bold text-gray-700">
+                    Let’s get you started
+                </div>
+                <div class="text-base text-gray-400 pt-6 w-full max-w-lg">
+                    Use the “Add new link” button to get started. Once you have more than one link, you can reorder and edit
+                    them. We’re here to help you share your profiles with everyone!
+                </div>
+            </div>
+            <div v-else class="mt-10 w-full" v-for="(link, index) in links" :key="index">
+                <LinkSelector @update-selected="setLinkPlatform" @set-link-value="setLinkUrl" @remove-link="removeLink"
+                    :link="link" :links="links" />
             </div>
         </div>
-        <div v-else class="mt-10 w-full" v-for="(link, index) in links" :key="index">
-            <LinkSelector @update-selected="setLinkPlatform" @set-link-value="setLinkUrl" @remove-link="removeLink"
-                :link="link" :links="links" />
+        <div class="mt-10 w-full flex items-center justify-end p-6 border-t border-gray-200" v-if="links.length">
+            <div class="w-fit-content">
+                <ButtonPrimary text="Save" :is-inside-nav="false" :click="submitLinks" @button-clicked="submitLinks" />
+            </div>
         </div>
-
-
-
-
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import ButtonSecondary from "@/components/ButtonSecondary.vue";
+import ButtonPrimary from "@/components/ButtonPrimary.vue";
 import LinksEmptyStateIcon from "@/assets/icons/LinksEmptyStateIcon.vue";
 import LinkSelector from "@/components/LinkSelector.vue";
 
 export default defineComponent({
     name: "CustomizeLinks",
     components: {
+        ButtonPrimary,
         ButtonSecondary,
         LinksEmptyStateIcon,
         LinkSelector
@@ -82,6 +89,9 @@ export default defineComponent({
         },
         removeLink(linkId: number): void {
             this.links = this.links.filter(link => link.id !== linkId);
+        },
+        submitLinks(): void {
+            console.log('submitting links', this.links)
         }
     }
 });
