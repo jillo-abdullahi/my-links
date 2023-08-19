6493d2e13a76207fc54b0cd1898fa5d635f4478a
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-08-19 13:14:56 +0300

    feat - add purple background to preview page

--- client/src/views/profilePreview/ProfilePreview.vue
+++ client/src/views/profilePreview/ProfilePreview.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="min-h-screen w-full -mt-12 ">
-        <div class="p-6">
+    <div class="relative min-h-screen w-full -mt-12 ">
+        <div class="absolute top-0 inset-x-0 bg-purple-700 rounded-b-2xl h-1/4"></div>
+        <div class="relative p-6 z-10">
             <ProfileNavBar />
         </div>
-        <div class="flex justify-center">
+        <div class="relative flex justify-center z-10">
             <ProfileLinks />
         </div>
     </div>
